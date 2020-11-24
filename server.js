f91b59d1715af25744bf2ce333e41a3b35855c48
halaalsawaeer <h.sawaeer@yahoo.com>
2020-11-25 00:21:27 +0200

    didn't finish

--- server.js
+++ server.js
@@ -1,78 +1,180 @@
-"use strict";
-//Aplication Depenencies (require)
+'use strict';
+// Application Dependencies
 const express = require('express');
+//CORS = Cross Origin Resource Sharing
 const cors = require('cors');
+const pg = require('pg');
+
+
+//DOTENV (read our enviroment variable)
 require('dotenv').config();
 const superagent = require('superagent');
-//application setup (port,server,use cors)
-const PORT = process.env.PORT || 3000;
-const server = express();
-server.use(cors());
-//Application Routes
-server.get('/location', locationHandlerFunc)
-    .then(data => {
+// Application Setup
+const PORT = process.env.PORT || 3030;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+const app = express();
+const client = new pg.Client(DATABASE_URL);
+
+app.use(cors());
+//Routes
+app.get('/location', checkLocation);
+app.get('/weather', getWeather);
+app.get('/trails', trailsHandler);
+app.get('*', notFoundPageHandler);
+
+function checkLocation(req,res) {
+    let city = req.query.city;
+    let SQL = `SELECT * FROM locations WHERE search_query=${city};`;
+    
+
+    client.query(SQL)
+    .then(result=>{
+        if (result.rowCount!==0 ){
+            
+            console.log(result);
+        res.send(result.rows);
+
+        }
+        
+        
     })
-server.get('/weather', weathHanadlerFun);
-server.get('*', allRoutes);
-server.use(errorHandler);
+    .catch(error=>errorHandler(error, req, res))
+}
+
+
+
+app.use(errorHandler);
 function locationHandlerFunc(req, res) {
-    // const locationData = require('./data/location.json');
-    // console.log(locationData);
     let cityName = req.query.city;
-    // console.log(req.query);
-}
-//----I put the location handler in a function because I want to get the returned data from it (superagent)
-//--and i put the superagent itself in a return beacause the return inseooide is refered to it but the one before it is refered to the function itself.
-//--------- I need the return whenever what I want to put inside needs more time to be returned 
-function getCityLocation(cityName) {
-    let GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
-    console.log(GEOCODE_API_KEY);
-    let url = `https://eu1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`;
-    return superagent.get(url)//bring me the data from url
-        .then(myData => {
-            let locObj = new Location(cityName, myData.body);//------ just the impotrtant data-------
-            return locObj//to the client
+    let locKey = process.env.LOCATION_KEY;
+    let url = `https://eu1.locationiq.com/v1/search.php?key=${locKey}&q=${cityName}&format=json`;
+    superagent.get(url)
+        .then(data => {
+            let locationData = new Location(cityName, data.body);
+            res.send(locationData);
         })
-    .catch(()=>{
-        errorHandler
-    })
+        .catch(() => {
+            errorHandler('Location .. Something went wrong!!', req, res);
+        });
 }
-// put the name of GEOCODE_API_KEY IN THE heruko;
-function Location(city, locData) {
+function Location(city, geoData) {
     this.search_query = city;
-    this.formatted_query = locData[0].display_name;
-    this.latitude = locData[0].lat;
-    this.longitude = locData[0].lon;
+    this.formatted_query = geoData[0].display_name;
+    this.latitude = geoData[0].lat;
+    this.longitude = geoData[0].lon;
 }
-// Weather.all = [];
-function Weather(day) {
-    this.forecast = day.weather.description;
-    this.time = day.time;
-    // Weather.all.push(this);
+function getWeather(req, res) {
+  let cityName = req.query.search_query;
+  let lat = req.query.latitude;
+  let lon = req.query.longitude;
+  let days = 5;
+  let wthrKye = process.env.WETHER_KEY;
+  let weatherDaily =[];
+  let weatherUrl =`https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&lat=${lat}&lon=${lon}&key=${wthrKye}&days=${days}`;
+  superagent.get(weatherUrl)
+  .then(data => {
+   weatherDaily = data.body.data.map(val => {
+       return new Weather(val);
+    });
+    res.send(weatherDaily);
+}).catch(() => {
+    errorHandler('Weather .. Something went wrong!!', req, res);
+  });
 }
-function weathHanadlerFun(req, res) {
-    let cityName = req.query.search_query;
-    let MY_WEATHER_API_KEY = process.env.MY_WEATHER_API_KEY;
-    let days = 8;
-    let latitude = req.query.latitude;
-    let lon = req.query.longitude;
-    let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?search_query=${cityName}&key=${MY_WEATHER_API_KEY}&16=${days}&longitude=${longitude}&latitude=${latitude}`;
-    superagent.get(weatherURL)
-        .then(weatherData => {
-        })
-    // let weather = require('./data/weather.json');
-    // let WeatherArr= weather.data.map(val =>{
-    //     return new Weather(val)
-    // });
-    // res.send(WeatherArr);
+function Weather(weatheData) {
+  this.forecast = weatheData.weather.description;;
+  this.time =  weatheData.datetime;
 }
-//handle all routes
-function allRoutes(req, res) {
-    res.status(404).send('not found')
+function trailsHandler(req, res) {
+  let lat = req.query.latitude;
+  let lon = req.query.longitude;
+  let triKye = process.env.TRAIL_KEY;
+  let triUrl = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=200&key=${triKye}`;
+  superagent.get(triUrl)
+  .then(data => {
+    let availableTrails = data.body.trails.map(val => {
+      return new Trail(val);
+    });
+    res.status(200).send(availableTrails);
+  }).catch(() => {
+    errorHandler('Trail .. Something went wrong!!', req, res);
+  });
 }
-function errorHandler(req, res) {
+function Trail(data) {
+  this.name = data.name;
+  this.location = data.location;
+  this.length = data.length;
+  this.stars = data.stars;
+  this.star_votes = data.starVotes;
+  this.summary = data.summary;
+  this.trail_url = data.url;
+  this.conditions = data.conditionStatus;
+  this.condition_date = data.conditionDate.split(' ')[0];
+  this.condition_time = data.conditionDate.split(' ')[1];
+}
+function notFoundPageHandler(req, res) {
+    res.status(404).send('Not Found');
+}
+function errorHandler(error, req, res) {
     res.status(500).send(error);
 }
-server.listen(PORT, () => {
-    console.log("Everything is good");
+client.connect()
+.then(()=>{
+    app.listen(PORT, () =>
+    console.log(`listening on ${PORT}`)
+    );
 })
+
+
+// const express = require('express');
+// const server = express();
+// require('dotenv').config();
+// const cors = require('cors');
+// server.use(cors());
+// const PORT = process.env.PORT || 3000;
+// server.get('/location', (req, res) => {
+//     const locationData = require('./data/location.json');
+//     console.log(locationData);
+//     const locationObj = new Location('Lynnwood', locationData);
+//     res.send(locationObj);//return
+// });
+// function Location(city, locData) {
+//     this.search_query = city;
+//     this.formatted_query = locData[0].display_name;
+//     this.latitude = locData[0].lat;
+//     this.longitude = locData[0].lon;
+// }
+// let weatherArray = [];
+// server.get('/weather', (req, res) => {
+//     const weatherData = require('./data/weather.json');
+//     weatherData.data.forEach(el1 => {
+//         let weatherObj = new Weather(el1);
+//     });
+//     res.send(weatherArray);
+// });
+// function Weather(weatherData) {
+//     this.forecast = weatherData.weather.description;
+//     this.time = weatherData.datetime;
+//     weatherArray.push(this);
+// }
+// server.get('*', (req, res) => {
+//     res.status(404).send('not found');
+// });
+// server.use((error, req, res) => {
+//     res.status(500).send(error);
+// });
+// server.listen(PORT, () => {
+//     console.log(`Listening on PORT: ${PORT}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
